44d2a83dcafc5107404d1f169b8ef06f238b15fa
Guillem Serra <guillemserralorenz@gmail.com>
2022-04-02 09:13:14 +0200

    basic project init

---
+++ Source/AVoid/AVoidGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AVoidGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AVOID_API AAVoidGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
